309f4366498db7c93e7165850dda9b29f8b22966
Ian <ian_tries@hotmail.com>
2020-10-13 21:19:20 -0300

    Fix ControladorUsuario
    No estaba registrándose correctamente al usuario en la bd. Arreglado

--- proyecto-limpio-spring-master/src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorUsuario.java
+++ proyecto-limpio-spring-master/src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorUsuario.java
@@ -1,11 +1,10 @@
 package ar.edu.unlam.tallerweb1.controladores;
 
-import java.util.Date;
-
 import javax.inject.Inject;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -16,33 +15,43 @@ import ar.edu.unlam.tallerweb1.servicios.ServicioUsuario;
 public class ControladorUsuario {
 	@Inject
 	private ServicioUsuario servicioUsuario;
-
-	public ModelAndView cargarUsuario(
+	
+	
+	@RequestMapping("/registroUsuario")
+	public ModelAndView registro() {
+		ModelMap modelo = new ModelMap();
+		return new ModelAndView("registroUsuario", modelo);
+	}
+	
+	@RequestMapping("/mostrarUsuarioRegistrado")
+	public ModelAndView usuarioRegistrado(
 			@RequestParam("nombre") String nombre,
 			@RequestParam("apellido") String apellido,
-			@RequestParam("sexo") char sexo,
+			@RequestParam("sexo") String sexo,
 			@RequestParam("email") String email,
 			@RequestParam("password") String password,
 			@RequestParam("passwordRepetido") String passwordRepetido
 			) {
+		
 		Usuario usuario = new Usuario();
-		usuario.setNombre(nombre);
-		usuario.setApellido(apellido);
-		usuario.setSexo(sexo);
-		usuario.setEmail(email);
-		usuario.setPassword(password);
-		Long idUsuario = servicioUsuario.guardarUsuario(usuario);
+			
+			usuario.setNombre(nombre);
+			usuario.setApellido(apellido);
+			usuario.setSexo(sexo);
+			usuario.setEmail(email);
+			usuario.setPassword(password);
+			usuario.setRol(2); // Por defecto se crean usuarios de tipo cliente (2)
+			Long id= servicioUsuario.guardarUsuario(usuario);
+		
 		ModelMap modelo = new ModelMap();
 		modelo.put("nombre", nombre);
 		modelo.put("apellido", apellido);
 		modelo.put("sexo", sexo);
 		modelo.put("email", email);
-		modelo.put("password", password);
-		modelo.put("passwordRepetido", passwordRepetido);
+
 	
 		return new ModelAndView("mostrarUsuarioRegistrado", modelo);
 		
 	}
-	
 	
 }
