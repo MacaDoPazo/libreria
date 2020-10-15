14de4465198e13a70c73b308edae9dd34ccc86da
MacaDoPazo <49454702+MacaDoPazo@users.noreply.github.com>
2020-10-15 11:09:43 -0300

    Delete ServicioCantLibros.java

--- proyecto-limpio-spring-master/src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioCantLibros.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.unlam.tallerweb1.servicios;
-
-import ar.edu.unlam.tallerweb1.modelo.CantidadLibros;
-
-public interface ServicioCantLibros {
-
-	Long guardarCantidadLibros(CantidadLibros cantidadLibros);
-
-}
