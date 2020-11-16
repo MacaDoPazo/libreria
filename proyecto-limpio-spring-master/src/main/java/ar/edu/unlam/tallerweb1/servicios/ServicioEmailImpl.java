aed3ae9db73b89663e7a86feed98b7111f53c237
Nahuel Pierotti <npie@unlam.edu.ar>
2020-11-15 21:03:23 -0300

    Corrige envio de email. Fuciona instanciando el ServicioEmailImpl pero no injectando el ServicioEmail

--- proyecto-limpio-spring-master/src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioEmailImpl.java
+++ proyecto-limpio-spring-master/src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioEmailImpl.java
@@ -8,19 +8,25 @@ import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+
+import org.springframework.stereotype.Service;
+
 import java.util.Properties;
+
+@Service
 public class ServicioEmailImpl implements ServicioEmail {
 
 	@Override
 	public String enviarEmail(String to, String subject, String body) throws Exception {
 		
-		String from="cumelenlibreria@gmal.com";
-		String pass="Cumelen2020";
+		String from="cumelenlibreria@gmail.com";
+		String pass="Cumelen2021";//"mfkaldfmigprxuiq";
 		String ret="ok";
 		Properties properties = new Properties();
 		properties.put("mail.smtp.host","smtp.gmail.com");
 		properties.put("mail.smtp.port","465");
 		properties.put("mail.smtp.auth", "true");
+		properties.put("mail.smtp.ssl.enable","true");
 		properties.put("mail.smtp.socketFactory.port", "465");
 		properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
 		
--- proyecto-limpio-spring-master/src/test/java/ar/edu/unlam/tallerweb1/persistencia/EnvioEmailTest.java
+++ proyecto-limpio-spring-master/src/test/java/ar/edu/unlam/tallerweb1/persistencia/EnvioEmailTest.java
@@ -1,21 +1,42 @@
 package ar.edu.unlam.tallerweb1.persistencia;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
+import javax.inject.Inject;
+
 import org.junit.Test;
 
 import ar.edu.unlam.tallerweb1.servicios.ServicioEmail;
+import ar.edu.unlam.tallerweb1.servicios.ServicioEmailImpl;
 public class EnvioEmailTest {
 
+	@Inject
 	private ServicioEmail servicioEmail;
+	
+	
+	private ServicioEmailImpl servicio;
 	
 	@Test
 	public void enviar()throws Exception {
-
-			String envio=servicioEmail.enviarEmail("npierotti@alumno.unlam.edu.ar", "We Miss U", "<html><h3>Te extrañamos</h3><br><p>Prueba Envio Email</p></html>"); 
+			/*this.servicio=new ServicioEmailImpl();
+			String envio=this.servicio.enviarEmail("nahuelpierotti@gmail.com", "We Miss U", "<html><h3>Te extrañamos</h3><br><p>Prueba Envio Email</p></html>"); 
+			*/
+			String envio=servicioEmail.enviarEmail("nahuelpierotti@gmail.com", "We Miss U", "<html><h3>Te extrañamos</h3><br><p>Prueba Envio Email</p></html>");
 			assertThat(envio).isEqualTo("ok");
 
 	}
 	
-	
+	public ServicioEmail getServicioEmail() {
+		return servicioEmail;
+	}
+	public void setServicioEmail(ServicioEmail servicioEmail) {
+		this.servicioEmail = servicioEmail;
+	}
+	public ServicioEmailImpl getServicio() {
+		return servicio;
+	}
+	public void setServicio(ServicioEmailImpl servicio) {
+		this.servicio = servicio;
+	}
 	
 }
